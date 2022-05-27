ec2ef209a5a7ace4fcead3c129258971d6c047ab
rayveness30 <rainzpedia@gmail.com>
2022-05-28 01:02:47 +0700

    UdinV2

---
+++ UdinV2/config.js
@@ -0,0 +1,61 @@
+/**
+* Thanks To
+* Allah S.W.T
+* Ortu
+* BOT INDO
+* Dika Ardnt
+* Sanzy Kawaii
+* FIRAA
+* Guntur
+* Dhika Ardiant
+* All Creator Bot
+* All Subscriber Ku
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+
+}
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'BagasPrdn',
+
+}
+
+// Other
+global.owner = ['6282129893899']
+global.premium = ['6282129893899']
+global.packname = 'UdinBot MD'
+global.author = 'Rayveness'
+global.sessionName = 'lolibot'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    eror: 'Yah Eror kak',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 2
+}
+global.thumb = fs.readFileSync('./lib/apri.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
